a7534225e1c626de2198dd6c5b1eff829c57ae0a
g9aerospace <g9militants@gmail.com>
2024-05-20 09:28:09 +0530

    Icons

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,7 @@
 import BricksNetwork from '~/media/bricks-network.png?jsx';
 import { component$ } from "@builder.io/qwik";
+import { HiHomeOutline, HiMapOutline } from "@qwikest/icons/heroicons";
+import { LuVote } from "@qwikest/icons/lucide";
 import headerstyles from "./header.module.css";
 
 export default component$(() => {
@@ -13,16 +15,19 @@ export default component$(() => {
           <li>
             <a href="/" rel="noopener noreferrer">
               Home
+              <HiHomeOutline />
             </a>
           </li>
           <li>
             <a href="/vote" rel="noopener noreferrer">
               Vote
+              <LuVote />
             </a>
           </li>
           <li>
             <a href="https://map.bricks-network.xyz" target="_blank" rel="noopener noreferrer">
               Map
+              <HiMapOutline />
             </a>
           </li>
         </nav>
