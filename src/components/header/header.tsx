216a1dfb60d2f5fb5a1ee4015d396060fdb8f134
g9aerospace <g9militants@gmail.com>
2024-05-20 08:53:53 +0530

    Updates

---
+++ src/components/header/header.tsx
@@ -0,0 +1,32 @@
+import BricksNetwork from '~/media/bricks-network.png?jsx';
+import { component$ } from "@builder.io/qwik";
+import headerstyles from "./header.module.css";
+
+export default component$(() => {
+  return (
+    <header class={headerstyles.header}>
+      <div class={[headerstyles.wrapper].join(" ")}>
+        <div class={headerstyles.logo}>
+            <BricksNetwork alt="BricksNetwork Icon"/>
+        </div>
+        <nav>
+          <li>
+            <a href="/" target="_blank" rel="noopener noreferrer">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="/vote" target="_blank" rel="noopener noreferrer">
+              Vote
+            </a>
+          </li>
+          <li>
+            <a href="https://map.bricks-network.xyz" target="_blank" rel="noopener noreferrer">
+              Map
+            </a>
+          </li>
+        </nav>
+      </div>
+    </header>
+  );
+});
